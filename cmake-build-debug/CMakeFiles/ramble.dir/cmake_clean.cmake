ecc62c1de186987d58b90f6c0c752a8936848cf4
sdrummeyjr <33046696+sdrummeyjr@users.noreply.github.com>
2019-03-08 08:10:11 -0500

    Constant Instructions
    Constant Instructions

---
+++ cmake-build-debug/CMakeFiles/ramble.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ramble.dir/main.c.obj"
+  "CMakeFiles/ramble.dir/chunk.c.obj"
+  "CMakeFiles/ramble.dir/memory.c.obj"
+  "CMakeFiles/ramble.dir/debug.c.obj"
+  "CMakeFiles/ramble.dir/value.c.obj"
+  "ramble.pdb"
+  "ramble.exe"
+  "ramble.exe.manifest"
+  "ramble.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ramble.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
